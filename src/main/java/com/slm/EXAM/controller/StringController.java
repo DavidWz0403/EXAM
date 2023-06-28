7edf0fc5cf1a67ed0ee8b5a1e6951a07023bdcb6
Startklar <wi22b033@technikum-wien.at>
2023-06-28 20:29:02 +0200

    feature/all

--- src/main/java/com/slm/EXAM/controller/StringController.java
+++ src/main/java/com/slm/EXAM/controller/StringController.java
@@ -1,2 +1,20 @@
-package com.slm.EXAM.controller;public class StringController {
+package com.slm.EXAM.controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class StringController {
+
+    @PostMapping("/modify")
+    public String modifyString(@RequestBody String input) {
+        StringBuilder sb = new StringBuilder(input.length());
+
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (i % 2 == 1) {
+                c = Character.toUpperCase(c);
+            }
+            sb.append(c);
+        }
+        return sb.toString();
+    }
 }
